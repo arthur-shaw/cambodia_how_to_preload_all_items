448f4e8e8abd3f902e1627a7bc2f8230928d65c8
Arthur Shaw <jashaw2002@gmail.com>
2025-04-02 00:34:16 -0400

    Add comments for understanding

--- data/extract_market_product_data.R
+++ data/extract_market_product_data.R
@@ -1,35 +1,44 @@
 # ingest text file
 text <- here::here("data", "raw_data.txt") |>
   readr::read_delim(
+    # delimters are line returns
     delim = "\r\n",
+    # name the resulting column
     col_names = "option"
   )
 
 # extract data
 df <- text |>
 	dplyr::mutate(
+    # outlet appears to have two parts
+    # first part is a 7-digit number
     outlet_id_pt1 = stringr::str_extract(
       string = option,
       pattern = "(?<=Outlet )[0-9]+?(?= .+? - Item)"
     ),
+    # second part is sometimes a number, sometimes text
     outlet_id_pt2 = stringr::str_extract(
       string = option,
       pattern = "(?<=Outlet [0-9]{7} ).+?(?= - Item)"
     ),
+    # item ID falls between hyphens
     item_id = stringr::str_extract(
       string = option,
       pattern = "(?<=Item )[0-9]{6}(?= - )"
     ),
+    # extract the description
+    # first, select the part after the item code
     item_desc = stringr::str_extract(
       string = option,
       pattern = "(?<=Item [0-9]{6} - ).+"
-      # "(?<=Item [])(?=\\.+)(?:\\.+)(\\d+)$"
     ),
+    # then, replace the ending dots and digits with missing
     item_desc = stringr::str_replace(
       string = item_desc,
       pattern = "\\.+\\d+$",
       replacement = ""
     ),
+    # option code
     option_code = stringr::str_extract(
       string = option,
       pattern = "(?<=..)\\d+$"
